e24fe3b6f27274de322632477a1d62110f4fe821
Brayan Quintero <josebrayanquinterocampos@gmail.com>
2024-10-25 14:10:50 -0700

    cambios

--- src/main/java/com/brayanquintero/libros/service/LibroServiceImpl.java
+++ src/main/java/com/brayanquintero/libros/service/LibroServiceImpl.java
@@ -1,19 +1,35 @@
 package com.brayanquintero.libros.service;
 
 import com.brayanquintero.libros.entity.Libro;
+import com.brayanquintero.libros.repository.LibroRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
+import java.util.Optional;
 
 public class LibroServiceImpl implements LibroService{
 
+    private LibroRepository libroRepository;
+
+    @Autowired
+    public LibroServiceImpl(LibroRepository libroRepository) {
+        this.libroRepository = libroRepository;
+    }
     @Override
-    public List<Librogit> getAll() {
-        return ;
+    public List<Libro> getAll() {
+        return libroRepository.findAll();
     }
 
     @Override
     public Libro getById(int id) {
-        return null;
+        Optional<Libro> optional = libroRepository.findById(id);
+        Libro libro = null;
+
+        if (optional.isPresent()) {
+            libro = optional.get();
+        }
+
+        return libro;
     }
 
     @Override
